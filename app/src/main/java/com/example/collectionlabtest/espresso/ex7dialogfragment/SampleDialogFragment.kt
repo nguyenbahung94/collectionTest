0a23725606ef6b47872e3a32d6f667783ae8625b
nguyenbahung94 <mr.hungcity@gmail.com>
2019-12-27 14:07:39 +0700

    done ex dialog fragment

---
+++ app/src/main/java/com/example/collectionlabtest/espresso/ex7dialogfragment/SampleDialogFragment.kt
@@ -0,0 +1,17 @@
+package com.example.collectionlabtest.espresso.ex7dialogfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.collectionlabtest.R
+
+class SampleDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_sample_ex7, container, false)
+
+}
