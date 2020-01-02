eca055ce6f227f838abb42d29c273258b1e4e199
nguyenbahung94 <mr.hungcity@gmail.com>
2020-01-02 16:49:20 +0700

    test shared preferences

---
+++ app/src/test/java/com/example/collectionlabtest/mockio/shared_preference/EmailValidatorTest.kt
@@ -0,0 +1,40 @@
+package com.example.collectionlabtest.mockio.shared_preference
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class EmailValidatorTest {
+
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com.uk"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"))
+    }
+
+    @Test fun emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"))
+    }
+
+    @Test fun emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"))
+    }
+
+    @Test fun emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""))
+    }
+
+    @Test fun emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null))
+    }
+
+}
