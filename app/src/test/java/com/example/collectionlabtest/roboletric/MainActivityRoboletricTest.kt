b7541055dfb5c22596aab5e99632328d913f0113
nguyenbahung94 <mr.hungcity@gmail.com>
2020-02-26 11:51:36 +0700

    remove code

--- app/src/test/java/com/example/collectionlabtest/roboletric/MainActivityRoboletricTest.kt
+++ app/src/test/java/com/example/collectionlabtest/roboletric/MainActivityRoboletricTest.kt
@@ -1,11 +1,2 @@
 package com.example.collectionlabtest.roboletric
 
-import android.os.Build
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-
-@Config(sdk = [Build.VERSION_CODES.O_MR1])
-@RunWith(RobolectricTestRunner::class)
-class MainActivityRoboletricTest {
-}
--- app/src/test/java/com/example/collectionlabtest/testing_basic/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/collectionlabtest/testing_basic/tasks/TasksViewModelTest.kt
@@ -1,15 +1,11 @@
 package com.example.collectionlabtest.testing_basic.tasks
 
-import android.os.Build
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
-
+/*
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
 class TasksViewModelTest {
 
-    /*@get:Rule
+    @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
 
@@ -57,5 +53,5 @@ class TasksViewModelTest {
 
         @Suppress("UNCHECKED_CAST")
         return data as T
-    }*/
-}
+    }
+}*/
